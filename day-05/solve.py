668486812a41fc8b1f84fb262b937a62de7f45a1
Sindre Mandelid Kvam <sindre.m.kvam@gmail.com>
2024-12-05 16:45:03 +0100

    Day 5 - part 1

--- day-05/solve.py
+++ day-05/solve.py
@@ -29,17 +29,48 @@ def get_page_numbers(data):
     return page_numbers
 
 
+def find_relevant_rules(pages: list, rules: list):
+    """Find rules that contain digits found in the pages"""
+
+    relevant_rules = []
+    pages = set(pages)
+
+    for rule in rules:
+
+        rule_set = set(rule)
+        # If the rule is a subset of the pages present (both rules apply)
+        if rule_set <= pages:  # subset operator
+            relevant_rules.append(rule)
+
+    return relevant_rules
+
+
 def solution1(data):
     """Solution to part 1"""
 
     rules = get_rules(data)
     page_numbers = get_page_numbers(data)
 
-    print(rules)
-    for pages in page_numbers:
-        print(pages)
+    middle_page_sum = 0
 
-    return
+    for pages in page_numbers:
+
+        relevant_rules = find_relevant_rules(pages, rules)
+        valid = True
+
+        for rule in relevant_rules:
+
+            if np.where(pages == rule[0])[0][0] < np.where(pages == rule[1])[0][0]:
+                # Valid line
+                pass
+            else:
+                valid = False
+
+        if valid:
+
+            middle_page_sum += pages[len(pages) // 2]
+
+    return middle_page_sum
 
 
 def solution2(data):
@@ -50,7 +81,7 @@ def solution2(data):
 
 if __name__ == "__main__":
     # Read the input file
-    with open("day-05/example_input.txt", "r", encoding="utf-8") as f:
+    with open("day-05/input.txt", "r", encoding="utf-8") as f:
         DATA = f.readlines()
 
     answ1 = solution1(DATA)
